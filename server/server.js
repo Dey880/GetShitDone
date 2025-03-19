7e7ba3f891ce5cf3f9a4590eda5bd0604a479e30
Dey <Day4y3@proton.me>
2025-03-19 08:46:53 +0100

    initialize project structure with server and frontend setup

---
+++ server/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+require('dotenv').config();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
+const app = express();
+
+const corsOptions = {
+    origin: process.env.FRONTEND_URL,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+}
+
+app.use(cord(corsOptions));
+
+app.use(cookieParser());
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+mongoose.connect(process.env.MONGODB_URL);
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('AAAAAAAAAAAHHHHHHHHHHHHHH');
+});
+
+app.listen(process.env.PORT)
