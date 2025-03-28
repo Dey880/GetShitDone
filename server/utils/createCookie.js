da37780446ea575d7fc0e61a3c5bd98f44ab673a
Dey <Day4y3@proton.me>
2025-03-28 11:01:04 +0100

    fix createCookie function to ensure proper cookie settings and formatting

--- server/utils/createCookie.js
+++ server/utils/createCookie.js
@@ -2,10 +2,10 @@ require("dotenv").config();
 
 async function createCookie(res, jwtToken) {
     res.cookie("jwt", jwtToken, {
-        httpOnly: true,
-        maxAge: 1000*60*60*24*5, //5 days
-        secure: process.env.NODE_ENV === "production",
-        sameSite: "Lax",
-    })};
+        httpOnly: true, 
+        maxAge: 1000*60*60*24*5, // 5 days
+        secure: process.env.NODE_ENV === "production"
+    });
+}
 
 module.exports = createCookie;
