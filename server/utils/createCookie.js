11d8a1af3b437ac9b67e09ab8f32ac324e119a44
Dey <Day4y3@proton.me>
2025-03-31 12:37:28 +0200

    fix: set 'sameSite' attribute to 'None' and enable 'secure' setting in createCookie function

--- server/utils/createCookie.js
+++ server/utils/createCookie.js
@@ -2,8 +2,10 @@ require("dotenv").config();
 
 async function createCookie(res, jwtToken) {
     res.cookie("jwt", jwtToken, {
+        httpOnly: true,
         maxAge: 1000*60*60*24*5, // 5 days
-        sameSite: "lax"
+        sameSite: "None",
+        secure: true,
     });
 }
 
