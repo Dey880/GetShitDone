cac91948ced805eec0d7ab3c9be8a8d86570ca3a
Dey <Day4y3@proton.me>
2025-03-31 11:30:19 +0200

    fix: add 'sameSite' attribute with value 'lax' in createCookie function

--- server/utils/createCookie.js
+++ server/utils/createCookie.js
@@ -2,7 +2,8 @@ require("dotenv").config();
 
 async function createCookie(res, jwtToken) {
     res.cookie("jwt", jwtToken, {
-        maxAge: 1000*60*60*24*5 // 5 days
+        maxAge: 1000*60*60*24*5, // 5 days
+        sameSite: "lax"
     });
 }
 
