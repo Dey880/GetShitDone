a5aecbc3a6feee76f7fe3fcb019135033c61a050
Dey <Day4y3@proton.me>
2025-03-31 11:23:15 +0200

    fix: remove 'httpOnly' setting from createCookie function

--- server/utils/createCookie.js
+++ server/utils/createCookie.js
@@ -2,9 +2,7 @@ require("dotenv").config();
 
 async function createCookie(res, jwtToken) {
     res.cookie("jwt", jwtToken, {
-        // httpOnly: true, 
-        maxAge: 1000*60*60*24*5, // 5 days
-        secure: process.env.NODE_ENV === "production",
+        maxAge: 1000*60*60*24*5 // 5 days
     });
 }
 
